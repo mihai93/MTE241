2fb055ec65093ea816abab402604081bf2e556c8
unknown <mrlistov@ECE-FPGA12.NEXUS.UWATERLOO.CA>
2014-11-13 16:14:24 -0500

    Created circle and bitmap functions

--- Project 4/main.c
+++ Project 4/main.c
@@ -10,19 +10,75 @@
 #define N	50
 #define CORNER	5
 
-void line(int x0, int y0, int x1, int y1, unsigned short circle[][N]) {
+void line(int x0, int y0, int x1, int y1, unsigned short dimArray[][N]) {
  
   int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
   int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
   int err = (dx>dy ? dx : -dy)/2, e2;
  
   for(;;){
-		circle[x0][y0] = FG;
+		dimArray[x0][y0] = FG;
     if (x0==x1 && y0==y1) break;
     e2 = err;
     if (e2 >-dx) { err -= dy; x0 += sx; }
     if (e2 < dy) { err += dx; y0 += sy; }
   }
+}
+
+void createCircle(unsigned short circle[][N])
+{
+	int x0, y0, f, dFx, dFy, x, y, radius;
+	
+	x0 = N/2;
+	y0 = N/2;
+	radius = N/2;
+	f = 1 - radius;
+	dFx = 0;
+	dFy = -2 * radius;
+	x = 0;
+	y = radius-1;
+	
+	line(x0, y - radius, x0, y0 + radius, circle);
+	line(x0 - radius, y0, x0 + radius, y0, circle);
+
+	while(x < y)
+	{
+		if(f >= 0)
+		{
+			y--;
+			dFy += 2;
+			f += dFy;
+		}
+		x++;
+		dFx += 2;
+		f += dFx + 1;
+		
+		line(x0 - x, y0 + y, x0 + x, y0 + y, circle);
+		line(x0 - x, y0 - y, x0 + x, y0 - y, circle);
+		line(x0 - y, y0 + x, x0 + y, y0 + x, circle);
+		line(x0 - y, y0 - x, x0 + y, y0 - x, circle);
+	}
+}
+
+void convToBitmap(unsigned short dimArray[][N], unsigned short bitmap[N])
+{
+	int i, j, index;
+	index = 0;
+	
+	for (i = 0;i<N;i++)
+	{
+		for (j = 0;j<N;j++)
+		{
+			if (dimArray[j][i] == FG)
+			{
+				bitmap[index] = dimArray[j][i];
+			}	else {
+				bitmap[index] = BG;
+			}
+			
+			index++;
+		}
+	}
 }
 
 int main( void ) {
@@ -36,56 +92,9 @@ int main( void ) {
 	SystemInit();
 	GLCD_Init();
 	GLCD_Clear(BG); 
-
-	x0 = N/2;
-	y0 = N/2;
-	radius = N/2;
-	f = 1 - radius;
-	ddF_x = 0;
-	ddF_y = -2 * radius;
-	x = 0;
-	y = radius-1;
 	
-	line(x0, y - radius, x0, y0 + radius, circle);
-	line(x0 - radius, y0, x0 + radius, y0, circle);
-
-	while(x < y)
-	{
-		if(f >= 0)
-		{
-			y--;
-			ddF_y += 2;
-			f += ddF_y;
-		}
-		x++;
-		ddF_x += 2;
-		f += ddF_x + 1;
-		
-		line(x0 - x, y0 + y, x0 + x, y0 + y, circle);
-		line(x0 - x, y0 - y, x0 + x, y0 - y, circle);
-		line(x0 - y, y0 + x, x0 + y, y0 + x, circle);
-		line(x0 - y, y0 - x, x0 + y, y0 - x, circle);
-	}
-	
-
-	
-	index = 0;
-	
-	for (i = 0;i<N;i++)
-	{
-		for (j = 0;j<N;j++)
-		{
-			if (circle[j][i] == FG)
-			{
-				circleBitmap[index] = circle[j][i];
-			}	else {
-				circleBitmap[index] = BG;
-			}
-			
-			index++;
-		}
-	}
-	
+	createCircle(circle);
+	convToBitmap(circle, circleBitmap);
 
 	GLCD_Bitmap (160-N/2, 120-N/2, N, N, (unsigned char*)circleBitmap);
 
