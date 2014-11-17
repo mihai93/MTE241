ad4a821decbda216c44c8a9fc7af76b51f157519
unknown <mrlistov@ECE-FPGA16.NEXUS.UWATERLOO.CA>
2014-11-17 15:25:31 -0500

    Update

--- Project 4/main.c
+++ Project 4/main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <RTL.h>
 #include <stdbool.h>
 #include <LPC17xx.h>
 #include "GLCD.h"
 
+#define OS_CLOCK     12000000
+#define OS_TICK        100000
 #define BG	White
 #define FG	Magenta
 #define N 64
 
+
+
 void createCircle(int xcenter, int ycenter)
 {
-	unsigned short chircle[N][N] = {BG};
 	unsigned short bmp[N] = {BG};
 	unsigned short blankBmp[N] = {BG};
-	int i, x0, y0, f, dFx, dFy, x, y, radius;
-	
+	int i, x0, y0, f, dFx, dFy, x, y, radius, x_pos, y_pos;
+	printf("Hello world!");
+	//os_mut_init(&mutex1);
+	printf("Hello world!2	");
+
+	x_pos = xcenter - N/2;
+	y_pos = ycenter - N/2;
 	x0 = N/2;
 	y0 = N/2;
 	radius = N/2;
@@ -61,15 +70,17 @@ void createCircle(int xcenter, int ycenter)
 		if (bmp[i] != FG)
 			bmp[i] = BG;
 			
-	GLCD_Bitmap (xcenter - N/2, ycenter - N/2, N, N, (unsigned char*)bmp);
+	GLCD_Bitmap (x_pos, y_pos, N, N, (unsigned char*)bmp);
 	
-	i = 1;
-	while(1)
-	{
-		GLCD_Bitmap (160 - N/2 + i, 120 - N/2 + i, N, N, (unsigned char*)blankBmp);
-		GLCD_Bitmap (160 - N/2 + i, 120 - N/2 + i, N, N, (unsigned char*)bmp);
-		i++;
-	}
+// 	i = 1;
+// 	while(1)
+// 	{
+// 		os_sem_wait(&sem, 0xffff);
+// 		GLCD_Bitmap (x_pos, y_pos, N, N, (unsigned char*)blankBmp);
+// 		os_sem_send(&sem);
+// 		GLCD_Bitmap (x_pos + i, y_pos + i, N, N, (unsigned char*)bmp);
+// 		i++;
+// 	}
 }
 
 int main( void ) {
